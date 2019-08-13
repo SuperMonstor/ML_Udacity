eef25aaf20dfb3a81f00568913d597f548ba6770
Sudarshan <senthilsudarshan@gmail.com>
2019-08-14 00:59:29 +0530

    Completed preprocessing

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template_complete.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template_complete.py
@@ -10,12 +10,11 @@ import pandas as pd #importing and managing datasets
 
 #Importing the dataset using pandas
 dataset = pd.read_csv('Data.csv') 
-
 #matrix of features
 X = dataset.iloc[:, :-1].values  #[rows, all columns except last one]
-
 #Dependant variable vector
 Y = dataset.iloc[:, 3].values
+
 
 #Taking care of missing data
 #Replace all missing data with mean of columns
@@ -25,8 +24,9 @@ imputer = Imputer(missing_values = np.nan, strategy = "mean", axis = 0)
 imputer = imputer.fit(X[:, 1:3])  #fitting columns required into imputer object
 X[:, 1:3] = imputer.transform(X[:, 1:3])    #changing the values with mean
 
+
 #Encoding categorical data
-''' Categorical data: Values are only from a finite set of variables '''
+#Categorical data: Values are only from a finite set of variables 
 #encode categorical data with numbers
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder() #create object
@@ -37,12 +37,15 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder() #create object
 Y = labelencoder_Y.fit_transform(Y)
 
+
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
 
 #Feature scaling (Values b/w -1 and +1)
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
+#Not required for Y variables as they are dependant
