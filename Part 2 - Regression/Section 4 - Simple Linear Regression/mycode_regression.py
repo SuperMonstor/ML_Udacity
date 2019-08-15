e51e6707245f15424e0aec5fb6cdcae25f2129fe
Sudarshan <senthilsudarshan@gmail.com>
2019-08-16 00:26:24 +0530

    visualisation for salary model

--- Part 2 - Regression/Section 4 - Simple Linear Regression/mycode_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/mycode_regression.py
@@ -2,8 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import matplotlib.pyplot as ply
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 #importing dataset
-dataset = pd.read_csv(')
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values #matrix of features
+Y = dataset.iloc[:, 1].values #dependacy vector
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
+
+'''  
+#Feature scaling (Values b/w -1 and +1)
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+'''
+
+#Fitting simple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#Predicting the test set results
+Y_pred = regressor.predict(X_test)
+ 
+#Visualising results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary Regression Model (Training dataset)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary Regression Model (Test dataset)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
